0d49fd7c9af3cd32e5ca532c3f59abcc78e4a386
guswo313 <103082900+guswo313@users.noreply.github.com>
2022-06-21 21:13:00 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,116 +1,40 @@
-package com.example.apps.foodrecommand2;
+package com.example.foodrocommender;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-    private GpsTracker gpsTracker;
 
-    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
-    private static final int PERMISSION_REQUEST_CODE = 100;
-    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();//액션바 생성
+        actionBar.hide();// 액션바 숨기기
 
-        if(!checkLocationServicesStatus()){
-            showDialogForLocationServiceSetting();
-        }else{
-            checkRunTimePermission();
-        }
+        Handler handler = new Handler();
 
-        findViewById(R.id.Btn1).setOnClickListener(new View.OnClickListener() {
+        ImageView whatfood = (ImageView)findViewById(R.id.whatfood);
+        Glide.with(this).load(R.raw.whatfood).into(whatfood);
+
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                gpsTracker = new GpsTracker(MainActivity.this);
-
-                double latitude = gpsTracker.getLatitide();
-                double longitude = gpsTracker.getLongitude();
-                Toast.makeText(MainActivity.this,"현재위치" + latitude + longitude,Toast.LENGTH_LONG).show();
-                startActivity(new Intent(MainActivity.this, SubActivity.class));
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(intent); // startActivity로 화면전환
+                finish();
             }
-        });
+        }, 5600);
 
     }
-
-
-    void checkRunTimePermission(){
-        int hasFineLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
-        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_COARSE_LOCATION);
-        if(hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED){
-
-        }else {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, REQUIRED_PERMISSIONS[0])) {
-                Toast.makeText(MainActivity.this, "앱 실행을 위해 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
-                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
-
-            } else {
-                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
-
-            }
-        }
-    }
-
-    private void showDialogForLocationServiceSetting(){
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("위치 서비스 비활성화");
-        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n" + "위치 설정을 수정하시겠습니까?");
-        builder.setCancelable(true);
-        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Intent callGPSSettingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
-            }
-        });
-        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-        builder.create().show();
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode){
-            case GPS_ENABLE_REQUEST_CODE:
-                if(checkLocationServicesStatus()){
-                    if(checkLocationServicesStatus()){
-                        Log.d("@@@", "result : GPS 활성화 완료");
-                        checkRunTimePermission();
-                        return;
-                    }
-                }
-                break;
-        }
-    }
-
-    public boolean checkLocationServicesStatus(){
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
 }
-
